e4a23ff15bc4ddf29844eece916cf4128d7079ac
bobyangxiaolin <bob.yang@bstek.com>
2017-05-16 09:52:24 +0800

    文件管理界面

--- cms-parent/cms-web/src/main/java/com/bstek/cms/web/site/file/FileController.java
+++ cms-parent/cms-web/src/main/java/com/bstek/cms/web/site/file/FileController.java
@@ -1,8 +1,31 @@
 package com.bstek.cms.web.site.file;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bstek.bdf3.dorado.jpa.JpaUtil;
+import com.bstek.cms.orm.File;
+import com.bstek.dorado.annotation.DataProvider;
+import com.bstek.dorado.annotation.DataResolver;
+import com.bstek.dorado.data.provider.Page;
 
 @Controller
 public class FileController {
 
+	@DataProvider
+	@Transactional(readOnly = true)
+	public void load(Page<File> page) {
+		JpaUtil
+			.linq(File.class)
+			.desc("createDate")
+			.paging(page);
+	}
+	
+	@DataResolver
+	@Transactional
+	public void save(List<File> files) {
+		JpaUtil.save(files);
+	}
 }
